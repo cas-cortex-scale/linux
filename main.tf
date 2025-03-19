d537286e4674cf97ebbc650af235e0a33f8326be
casbs77 <svc-casbs7@paloaltonetworks.com>
2025-03-19 18:17:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "default_identity_pool_id" {
   #checkov:skip=CKV2_AWS_34:Non-sensitive parameter
-  name      = "/cognito/default_identity_pool_id_${var.unique_tag}"
+  name      = "/cognito/default_identity_pool_id_${var.unique_tag}/"
   type      = "String"
   value     = aws_cognito_identity_pool.identity_pool.id
   overwrite = true
